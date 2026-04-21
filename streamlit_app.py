8b465d3474e95a17b20709f7b093ad4cb83e04d5
kaiflury <kaiandreflury@gmail.com>
2026-04-21 13:11:55 +0200

    Rename expander to 'Data' in Streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,10 @@ st.title("🚗 Used-Car Price Estimator")
 
 st.info("This is an App built for a project in our Computer Science class. We use Machine Learning and Multiple Regression Analysis of used car price data in order to estimate the price of any used car.")
 
-with st.expander():
+with st.expander("Data"):
   st.subheader("**Raw Data**")
   df = pd.read_csv("https://raw.githubusercontent.com/kaiflury/KF_streamlit_CS_Project_Test/refs/heads/master/car_data.csv")
   df
+
+  st.write("**X**")
+  X = df.drop("
