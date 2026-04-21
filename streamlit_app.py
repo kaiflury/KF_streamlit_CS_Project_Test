5db69faf1cb623f3b70347b3f7c8d2ff4eff58e5
kaiflury <kaiandreflury@gmail.com>
2026-04-21 13:08:57 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,7 @@ st.title("🚗 Used-Car Price Estimator")
 
 st.info("This is an App built for a project in our Computer Science class. We use Machine Learning and Multiple Regression Analysis of used car price data in order to estimate the price of any used car.")
 
-df = pd.read_csv("https://raw.githubusercontent.com/kaiflury/KF_streamlit_CS_Project_Test/refs/heads/master/car_data.csv")
-df
+with st.expander():
+  st.subheader("**Raw Data**")
+  df = pd.read_csv("https://raw.githubusercontent.com/kaiflury/KF_streamlit_CS_Project_Test/refs/heads/master/car_data.csv")
+  df
